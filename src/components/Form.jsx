26e01df169fc0390b0c219379f28ade166e493e3
Alan Webb <webbalan91939@gmail.com>
2026-07-25 13:23:35 +0100

    Update Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,10 @@
 const Form = () => {
 	return (
 		<form className="grid grid-cols-1 md:grid-cols-2 space-y-2">
-			<input type="text" name="name" placeholder="Enter your full name" />
-			<input type="email" name="email" placeholder="Enter your email address" />
-			<input type="tel" name="tel" placeholder="Enter your contact number" />
-			<input type="text" name="industry" placeholder="Enter your industry" />
+			<input type="text" name="name" placeholder="Full name" />
+			<input type="email" name="email" placeholder="Email" />
+			<input type="tel" name="tel" placeholder="Phone number" />
+			<input type="text" name="industry" placeholder="Industry" />
 			<textarea placeholder="message" className="md:col-span-2"></textarea>
 		</form>
 	);

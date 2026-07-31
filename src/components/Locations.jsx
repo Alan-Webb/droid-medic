1343e72c6594784f4aa749be9cc3113798058960
Alan Webb <webbalan91939@gmail.com>
2026-07-31 12:52:16 +0100

    Update Locations component

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -4,7 +4,13 @@ const Locations = () => {
 	return (
 		<div
 			style={{backgroundImage: `url("${LocationsBG}")`}}
-			className="py-20 bg-cover bg-center"></div>
+			className="py-20 bg-cover bg-center">
+			<div className="max-w-7xl mx-auto">
+				<h2 className="text-center text-4xl font-bold text-amber-500 tracking-wider">
+					Our Store Locations
+				</h2>
+			</div>
+		</div>
 	);
 };
 

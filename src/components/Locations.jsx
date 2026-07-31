0989feced5cd02af7f97222a72303045671e6714
Alan Webb <webbalan91939@gmail.com>
2026-07-31 03:20:10 +0100

    Update Locations & assets

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -1,8 +1,11 @@
+import LocationsBG from "../assets/locationsBG.webp";
 
 const Locations = () => {
-  return (
-    <div>Locations</div>
-  )
-}
+	return (
+		<div
+			style={{backgroundImage: `url("${LocationsBG}")`}}
+			className="py-20 bg-cover bg-center"></div>
+	);
+};
 
-export default Locations
+export default Locations;

ab891d0a9a2ad6d25d68dca09b4b70d7726b5865
Alan Webb <webbalan91939@gmail.com>
2026-08-01 10:47:19 +0100

    Update Locations component

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -1,6 +1,15 @@
 import LocationsBG from "../assets/locationsBG.webp";
 
-const cities = ["City A", "City B", "City C", "City D", "City E", "City F"];
+const cities = [
+	"City A",
+	"City B",
+	"City C",
+	"City D",
+	"City E",
+	"City F",
+	"City G",
+	"City H",
+];
 
 const Locations = () => {
 	return (
@@ -14,6 +23,13 @@ const Locations = () => {
 				</h2>
 			</div>
 			{/* LOCATION CARDS */}
+			<div className="max-w-7xl mx-auto mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+				{cities.map((city, index) => (
+					<div key={index} className="bg-amber-700/85 rounded-lg p-4 m-2">
+						<h3 className="text-xl font-semibold">{city}</h3>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };

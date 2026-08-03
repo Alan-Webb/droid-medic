c2309c3b1c3ff30924d49b55e091f3cb0ddeabe2
Alan Webb <webbalan91939@gmail.com>
2026-08-03 05:17:34 +0100

    Update Locations component

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -6,7 +6,6 @@ const cities = [
 	"New York",
 	"Copenhagen",
 	"Vienna",
-	"Melbourne",
 	"Zurich",
 	"Sydney",
 	"Vancouver",
@@ -24,12 +23,14 @@ const Locations = () => {
 				</h2>
 			</div>
 			{/* LOCATION CARDS */}
-			<div className="max-w-7xl mx-auto mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+			<div className="max-w-7xl mx-auto mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
 				{cities.map((city, index) => (
 					<div
 						key={index}
 						className="bg-amber-700/85 rounded-lg p-4 m-2 text-center shadow-md">
-						<h3 className="text-stone-300 text-xl font-semibold">{city}</h3>
+						<h3 className="text-stone-300 text-2xl font-bold uppercase tracking-wider">
+							{city}
+						</h3>
 					</div>
 				))}
 			</div>

96aa04e7f549a4c049f662351b5c275b983d65fd
Alan Webb <webbalan91939@gmail.com>
2026-09-07 09:53:06 +0100

    Create & update FaqItem component

---
+++ src/components/FaqItem.jsx
@@ -0,0 +1,23 @@
+import {useState} from "react";
+import {FaChevronCircleDown, FaChevronCircleUp} from "react-icons/fa";
+
+const FaqItem = ({question, answer}) => {
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<div>
+			<button
+				onClick={() => setIsOpen(!isOpen)}
+				className="flex justify-between px-2 items-center">
+				<span className="text-xl">{question}</span>
+				{isOpen ? (
+					<FaChevronCircleUp className="text-xl text-amber-700 m-2 cursor-pointer" />
+				) : (
+					<FaChevronCircleDown className="text-xl text-amber-700 m-2 cursor-pointer" />
+				)}
+			</button>
+			{isOpen && <p className="pl-4">{answer}</p>}
+		</div>
+	);
+};
+
+export default FaqItem;
--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -1,10 +1,6 @@
 import {useState} from "react";
-import {
-	FaChevronCircleDown,
-	FaChevronCircleUp,
-	FaEnvelope,
-	FaPhone,
-} from "react-icons/fa";
+import FaqItem from "./FaqItem";
+import {FaEnvelope, FaPhone} from "react-icons/fa";
 
 const questionsAnswers = [
 	{
@@ -58,25 +54,6 @@ const questionsAnswers = [
 			"Yes! We offer custom bodywork, high-durability thermal powder coating, custom decal application, and weather-proofing treatments.",
 	},
 ];
-
-const FaqItem = ({question, answer}) => {
-	const [isOpen, setIsOpen] = useState(false);
-	return (
-		<div>
-			<button
-				onClick={() => setIsOpen(!isOpen)}
-				className="flex justify-between px-2 items-center">
-				<span className="text-xl">{question}</span>
-				{isOpen ? (
-					<FaChevronCircleUp className="text-xl text-amber-700 m-2 cursor-pointer" />
-				) : (
-					<FaChevronCircleDown className="text-xl text-amber-700 m-2 cursor-pointer" />
-				)}
-			</button>
-			{isOpen && <p className="pl-4">{answer}</p>}
-		</div>
-	);
-};
 
 const Faqs = () => {
 	return (
